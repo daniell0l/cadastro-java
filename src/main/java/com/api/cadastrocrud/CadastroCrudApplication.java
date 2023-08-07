fb89d53d801fcd9f688656952b59b2c8e1920db2
daniell0l <daniel_l0l@hotmail.com>
2023-08-07 01:21:26 -0300

    feat iniciando projeto java

---
+++ src/main/java/com/api/cadastrocrud/CadastroCrudApplication.java
@@ -0,0 +1,13 @@
+package com.api.cadastrocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/cadastrocrud/CadastroCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.cadastrocrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
